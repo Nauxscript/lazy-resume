a37902f15fcdbbf42e116ec62f7ade7cc9b7688b
naux-home <naux-home@mutekey.com>
2021-11-13 21:24:21 +0800

    coding style

--- src/index.js
+++ src/index.js
@@ -3,5 +3,28 @@ import "./style/index.css"
 if (process.env.NODE_ENV !== 'production') {
   require('../index.html')
 }
-console.dir('123');
-console.log('??')
+
+let mode = 'light'
+
+// darkmode
+window.onload = function() {
+  console.log('ready')
+  let m = localStorage.getItem('mode')
+  if (m && m === 'dark') {
+    mode = 'dark'
+    document.documentElement.classList.add('dark')
+  }
+}
+const darkModeBtn = document.getElementById('toggleDarkMode')
+darkModeBtn.addEventListener('click', function(e){
+  if (mode === 'dark') {
+    mode = 'light'
+    document.documentElement.classList.remove('dark')
+    localStorage.setItem('mode', 'light')
+  } else {
+    mode = 'dark'
+    document.documentElement.classList.add('dark')
+    localStorage.setItem('mode', 'dark')
+  }
+  
+})

48f10ea163945f3dd69e3b8da122a149be4cb3ff
noah <xuannoah@gmail.com>
2021-11-15 17:59:37 +0800

    build test

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: 'bundel.[hash].js',
     clean: true,
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, '../dist')
   },
   plugins: [
     new HtmlWebpackPlugin({
